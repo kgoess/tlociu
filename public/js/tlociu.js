deb759966d3b53f84b3346586c2399babb655cdd
Kevin M. Goess <kevin@goess.org>
2026-07-21 22:41:53 +0000

    Add markdown to the user notes.
    Also putting the javascript into DOMContentLoaded because that seems like a good idea.
    
    Also adding a touch of color to offset the stuff the user has written from the
    data from tmdb.org, because now with markdown it seems like the page can get
    jumbled and confusing.

--- public/js/tlociu.js
+++ public/js/tlociu.js
@@ -1,4 +1,27 @@
 
-function toggleExpandableText(element) {
-  element.classList.toggle('clamped');
-}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    function toggleExpandableText(element) {
+        element.classList.toggle('clamped');
+    }
+
+    function attachExpandableText() {
+        const elements = document.querySelectorAll('.expandable-text');
+        elements.forEach(element => {
+            element.addEventListener('click', (event) => { toggleExpandableText(element) });
+        });
+    }
+
+    function renderMarkdowns() {
+        const markdownEls = document.querySelectorAll('.markdown');
+
+        markdownEls.forEach(element => {
+            const renderedHtml = marked.parse(element.innerHTML);
+            element.innerHTML = renderedHtml;
+        });
+    }
+
+    attachExpandableText();
+    renderMarkdowns();
+});
